259f223016e15cef4d07e2c8d5e78ed3e61af470
Colby Neves <colby.neves@smail.astate.edu>
2023-05-19 15:50:38 -0500

    user name changes made

--- src/models/XboxModel.ts
+++ src/models/XboxModel.ts
@@ -7,9 +7,10 @@ async function xboxAuthModel(email: string, password: string): Promise<void> {
 
     console.log(response);
 
-    const { xuid } = response as CredentialsAuthenticateInitialResponse;
+    const { xuid, display_claims } = response as CredentialsAuthenticateInitialResponse;
+    const { gtg } = display_claims as xboxUserInfo;
 
-    console.log(xuid);
+    console.log(xuid, gtg);
 
     return;
 
--- src/types/xboxTypes.d.ts
+++ src/types/xboxTypes.d.ts
@@ -1,8 +1,7 @@
 type xboxUserInfo = {
 
     xuid: string;
-    display_claims: {
-        gtg: string;
-    }
+    gtg: string;
+
 
 }
