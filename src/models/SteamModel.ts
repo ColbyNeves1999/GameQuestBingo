d91b239aaf42472a21c15e325f6ab418152b3636
Colby Neves <colby.neves@smail.astate.edu>
2023-08-30 14:45:49 -0500

    Changes to optimize steamGameGrab

--- src/models/SteamModel.ts
+++ src/models/SteamModel.ts
@@ -3,14 +3,20 @@ import { AppDataSource } from '../dataSource';
 import { Game } from "../entities/Game";
 import { SteamAchieve } from "../entities/SteamAchievement";
 
-import { getGameByName, setSteamID } from "./GameModel";
+import { getGameByName } from "./GameModel";
 
 const steamAchievementRepository = AppDataSource.getRepository(SteamAchieve);
 
 //ATTENTION!!! DO NOT RUN steamGameGrab. THE CODE DOES IT ON ITS OWN. TAKES OVER AN HOUR TO COMPLETE.
 //Grabs every game on steam and it's achievements, that isn't already in the database.
 async function steamGameGrab(): Promise<void> {
+
+    //Backend timing for troubleshooting purposes
+    const start = getCurrentTime();
+
     console.log("Checking steam for games and achievements");
+
+    //Fetches list of games on Steam
     const fetchResponse = await fetch('https://api.steampowered.com/ISteamApps/GetAppList/v2/', {
         method: 'GET',
         headers: {
@@ -18,33 +24,35 @@ async function steamGameGrab(): Promise<void> {
         }
     });
 
+    //Breaks down fetch response into array of games
     const resJson = await fetchResponse.json();
     const { applist } = resJson as steamAppList;
     const { apps } = applist as steamApps;
 
-    for (let i = 0; i < apps.length; i++) {
-
-        //Gets name of game, and creates standard lower casing to make game searching reliable
-        const { appid, name } = apps[i] as steamGameData;
-        const lowerName = name.toLocaleLowerCase();
+    //Runs each member of the array in parallel, breaking it down into IDs and names
+    const results = await Promise.all(apps.map(async (app) => {
+        const { appid, name } = app;
+        const lowerName = name.toLowerCase();
         const isItAlreadyHere = await getGameByName(lowerName);
-        let platform = [];
-        platform[0] = "6";
+        return { appid, isItAlreadyHere };
+    }));
 
-        //Will take existing games and add steam ids to them and grabs their achievements
-        if (isItAlreadyHere) {
+    const gamesToFetchAchievements = results.filter((result) => result.isItAlreadyHere);
 
-            await setSteamID(appid, isItAlreadyHere);
-            await steamAchievementGrab(appid, isItAlreadyHere);
+    //Checks steam for achievements
+    const achievementsPromises = gamesToFetchAchievements.map(async (result) => {
+        console.log(result.appid, " ", result.isItAlreadyHere);
+        await steamAchievementGrab(result.appid, result.isItAlreadyHere);
+    });
 
-        }
+    await Promise.all(achievementsPromises);
 
-    }
-
-    //Used to verify achievement grabbing has been completed
     console.log("I've grabbed all the achievements I can");
 
-    return;
+    const stop = getCurrentTime();
+    console.log("Start time: ", start);
+    console.log("Stop time: ", stop);
+
 }
 
 async function setSteamAchievements(achievement: string, game: Game): Promise<void> {
@@ -71,11 +79,11 @@ async function getSteamAchievement(achievement: string, game: Game): Promise<Ste
 //Grabs achievements for games on steam. Due to the inconsistant data Steam can and does return,
 //there are try/catches constantly. Returned data can be wildly vary, and therefore fail, at any point
 async function steamAchievementGrab(appid: string, steamGame: Game): Promise<void> {
-
     const steam_key = process.env.STEAM_API_KEY;
 
     try {
-        //Steam api found on TF2 wiki (Why is it classified on the TF2 wiki but not their website?)
+
+        //Fetching data from steam api found on TF2 wiki (Why is it classified on the TF2 wiki but not their website?)
         const fetchResponse = await fetch(`http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v0002/?key=${steam_key}&appid=${appid}&l=english&format=json`, {
             method: 'GET',
             headers: {
@@ -83,46 +91,39 @@ async function steamAchievementGrab(appid: string, steamGame: Game): Promise<voi
             }
         });
 
-        try {
-            //Grabs achievements from deconstructed data
-            const resJson = await fetchResponse.json();
-            const { game } = resJson as steamAchievementPercentage;
-            const { availableGameStats } = game;
-
-            try {
-                const { achievements } = availableGameStats;
-
-                try {
-                    for (let i = 0; i < achievements.length; i++) {
-
-                        try {
-                            //Makes sure duplicate achievements for steam aren't entered in
-                            const doesAchievementExist = await getSteamAchievement(achievements[i].displayName, steamGame);
-                            console.log(doesAchievementExist);
-                            if (!doesAchievementExist) {
-                                await setSteamAchievements(achievements[i].displayName, steamGame);
-                                console.log(achievements[i].displayName);
-                            }
-                        } catch {
-                            return;
-                        }
-                    }
-                } catch {
-                    return;
-                }
-
-            } catch {
-                return;
-            }
-        } catch {
+        if (!fetchResponse.ok) {
+            console.error(`Error fetching schema for app ${appid}`);
             return;
         }
-    } catch {
-        return;
+
+        //Breaks down fetch response into array of achievements
+        const resJson = await fetchResponse.json();
+        const { game } = resJson as steamAchievementPercentage;
+        const { availableGameStats } = game;
+        const { achievements } = availableGameStats;
+
+        //This breaks down all achievements per game and adds them to the database
+        await Promise.all(achievements.map(async (achievement) => {
+            try {
+                const doesAchievementExist = await getSteamAchievement(achievement.displayName, steamGame);
+                if (!doesAchievementExist) {
+                    await setSteamAchievements(achievement.displayName, steamGame);
+                    console.log(achievement.displayName);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }));
+
+    } catch (error) {
+        console.error(error);
     }
+}
 
-    return;
-
+//This only exists for backend testing to see how long the Steam grabs take
+function getCurrentTime(): string {
+    const now = new Date();
+    return now.toLocaleTimeString();
 }
 
 export { steamGameGrab, steamAchievementGrab, setSteamAchievements, getSteamAchievement };
